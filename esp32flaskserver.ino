6f2eff1a36d36195506ba3bf7407a31814370b47
AGBAJE DANIEL <63917027+danzyblaze@users.noreply.github.com>
2023-06-22 18:23:10 +0100

    Update esp32flaskserver.ino
    ALTERNATIVE CODE IF CAMERA IS FAULTY

--- esp32flaskserver.ino
+++ esp32flaskserver.ino
@@ -1,57 +1,22 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
+#include <String.h>
 #include <esp_camera.h>
+#define LED_BUILTIN 4
 
 // Replace with your network credentials
 const char* ssid = "ANONYMOUS";
 const char* password = "yamarita";
 
 // Replace with your Flask server IP address or hostname
-const char* serverUrl = "http://localhost:5000/recognize_faces";
+const char* serverUrl = "http://192.168.0.102:5000/get_last_response";
 
 // Pin connected to the lock and buzzer
-const int lockPin = 13;
-const int buzzerPin = 12;
+const int lockPin = 13; //Pin connected to solenoid lock
+const int buzzerPin = 12; //Pin connected to buzzer
+const int triggerPin = 14; // Pin to receive the signal from arduino uno
 
-// Camera settings
-const int cameraCaptureTimeout = 5000;  // Timeout for capturing a photo in milliseconds
-
-// Base64 encoding function
-String base64Encode(const uint8_t* data, size_t length) {
-  const char* base64Table = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-  String encodedData = "";
-  size_t dataIndex = 0;
-  uint8_t input[3];
-  uint8_t output[4];
-  while (length--) {
-    input[dataIndex++] = *(data++);
-    if (dataIndex == 3) {
-      output[0] = (input[0] & 0xFC) >> 2;
-      output[1] = ((input[0] & 0x03) << 4) | ((input[1] & 0xF0) >> 4);
-      output[2] = ((input[1] & 0x0F) << 2) | ((input[2] & 0xC0) >> 6);
-      output[3] = input[2] & 0x3F;
-      for (size_t i = 0; i < 4; i++) {
-        encodedData += base64Table[output[i]];
-      }
-      dataIndex = 0;
-    }
-  }
-  if (dataIndex > 0) {
-    for (size_t i = dataIndex; i < 3; i++) {
-      input[i] = 0;
-    }
-    output[0] = (input[0] & 0xFC) >> 2;
-    output[1] = ((input[0] & 0x03) << 4) | ((input[1] & 0xF0) >> 4);
-    output[2] = ((input[1] & 0x0F) << 2) | ((input[2] & 0xC0) >> 6);
-    for (size_t i = 0; i < dataIndex + 1; i++) {
-      encodedData += base64Table[output[i]];
-    }
-    while (dataIndex++ < 3) {
-      encodedData += '=';
-    }
-  }
-  return encodedData;
-}
+String cmp;
 
 void setup() {
   // Initialize serial communication
@@ -65,84 +30,66 @@ void setup() {
   }
   Serial.println("Connected to WiFi");
 
-  // Configure the camera
-  camera_config_t cameraConfig;
-  cameraConfig.ledc_channel = LEDC_CHANNEL_0;
-  cameraConfig.ledc_timer = LEDC_TIMER_0;
-  cameraConfig.pin_d0 = 5;
-  cameraConfig.pin_d1 = 18;
-  cameraConfig.pin_d2 = 19;
-  cameraConfig.pin_d3 = 21;
-  cameraConfig.pin_d4 = 36;
-  cameraConfig.pin_d5 = 39;
-  cameraConfig.pin_d6 = 34;
-  cameraConfig.pin_d7 = 35;
-  cameraConfig.pin_xclk = 0;
-  cameraConfig.pin_pclk = 22;
-  cameraConfig.pin_vsync = 25;
-  cameraConfig.pin_href = 23;
-  cameraConfig.pin_sscb_sda = 26;
-  cameraConfig.pin_sscb_scl = 27;
-  cameraConfig.pin_pwdn = 32;
-  cameraConfig.pin_reset = -1;
-  cameraConfig.xclk_freq_hz = 20000000;
-  cameraConfig.pixel_format = PIXFORMAT_JPEG;
-  
-  // Initialize the camera
-  esp_err_t error = esp_camera_init(&cameraConfig);
-  if (error != ESP_OK) {
-    Serial.printf("Camera initialization failed with error 0x%x", error);
-    return;
-  }
-
-  // Set lock and buzzer pins as outputs
+  // Set lock, buzzer, and trigger pins as outputs
   pinMode(lockPin, OUTPUT);
   pinMode(buzzerPin, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(triggerPin, INPUT_PULLUP);
 }
 
 void loop() {
-  // Take a photo
-  camera_fb_t* fb = esp_camera_fb_get();
-  if (!fb) {
-    Serial.println("Failed to capture photo");
-    return;
-  }
+digitalWrite(lockPin,0);
+digitalWrite(buzzerPin,0); 
+  // Check the state of the trigger pin
 
-  // Create a HTTPClient object
-  HTTPClient http;
+  if (digitalRead(triggerPin) == 1) {
+    digitalWrite(LED_BUILTIN,1);
+
   
-  // Send POST request with the base64-encoded photo to the server
-  String encodedPhoto = base64Encode(fb->buf, fb->len);
-  http.begin(serverUrl);
-  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-  int httpResponseCode = http.POST("image=" + encodedPhoto);
-  
-  // Check the response from the server
-  if (httpResponseCode == HTTP_CODE_OK) {
-    String response = http.getString();
-    Serial.println("Response: " + response);
-    
-    // Check if the response is authorized or unauthorized
-    if (response == "Authorized: Faces match.") {
-      digitalWrite(lockPin, HIGH);   // Trigger the lock
-      delay(3000);                   // Lock for 3 seconds
-      digitalWrite(lockPin, LOW);    // Release the lock
-    } else {
-      digitalWrite(buzzerPin, HIGH); // Trigger the buzzer
-      delay(3000);                   // Buzz for 3 seconds
-      digitalWrite(buzzerPin, LOW);  // Stop buzzing
     }
-  } else {
-    Serial.print("Error: ");
+    // Create a HTTPClient object
+    HTTPClient http;
+    
+    
+    // Send GET request to the server
+    http.begin(serverUrl);
+    int httpResponseCode = http.GET();
     Serial.println(httpResponseCode);
-  }
-  
-  // Free the memory used by the photo buffer
-  esp_camera_fb_return(fb);
-  
-  // End the HTTP request
-  http.end();
 
-  // Delay before taking the next photo
-  delay(5000);
+    // Check the response from the server
+    if (httpResponseCode == HTTP_CODE_OK) {
+      // String response = http.getString();
+      // Serial.println("Response: " + response);
+      // cmp = String("{\"response\":\"Authorized: Faces match.\"}");
+      // Serial.println(response);
+      // Serial.println(cmp);
+      // // Check if the response is authorized or unauthorized
+      // if (cmp.equals(cmp)) {
+      
+        Serial.println("ACCESS IS GRANTED "); 
+        digitalWrite(lockPin,1);   // Trigger the lock
+        delay(3000);                   // Lock for 3 seconds
+        digitalWrite(lockPin,0); // Release the lock
+         
+      // } 
+    }else {
+        Serial.println("ACCESS IS NOT-GRANTED "); 
+        digitalWrite(lockPin,0);
+        delay(3000); 
+        digitalWrite(buzzerPin,1); // Trigger the buzzer
+        delay(3000);                   // Buzz for 3 seconds
+        digitalWrite(buzzerPin,0); // Stop buzzing
+       
+      } 
+
+
+    // End the HTTP request
+    http.end();
+
+    // Delay before sending the next request
+    delay(5000);
+  } else {
+    digitalWrite(LED_BUILTIN,0); // Code does nothing and switches off the LED when shouldRunCode is false
+    delay(1000); //delay by 1 second
+  }
 }
